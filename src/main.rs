922be44a97c5cb24220bee9c7c81f8d501afa336
Cyryl Płotnicki <cyplo@cyplo.net>
2018-07-24 14:12:28 +0100

    Add scaffolding of the types needed by the test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Instant;
+use std::time::Duration;
+
+trait Clock {
+    fn now(&self) -> Instant;
+}
+
+struct FakeClock;
+
+impl FakeClock {
+    fn with_time(now: Instant) -> Self {
+        FakeClock{}
+    }
+}
+
+impl Clock for FakeClock {
+    fn now(&self) -> Instant {
+        unimplemented!()
+    }
+}
+
+fn format_time_difference(clock1: impl Clock, clock2: impl Clock) -> String {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod should {
+
+    use super::*;
+
+    #[test]
+    fn handle_seconds() {
+        let base_time = Instant::now();
+        let clock1 = FakeClock::with_time(base_time);
+        let clock2 = FakeClock::with_time(base_time + Duration::from_secs(32) );
+
+        let formatted_time_difference = format_time_difference(clock1, clock2);
+
+        assert_eq!("32 seconds ago", formatted_time_difference);
+    }
+
 }
